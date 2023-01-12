3a23c7465cc2d40d6dd57604718d3ffa3d69fc5f
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2023-01-12 16:09:55 +0530

    13 add product form generated

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrls: ['./seller-add-product.component.css']
+  styleUrls: ['./seller-add-product.component.css'],
 })
 export class SellerAddProductComponent {
-
+  submit(data: object) {
+    console.log(data);
+  }
 }
