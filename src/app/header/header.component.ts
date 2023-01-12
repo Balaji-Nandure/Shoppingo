9232512d32a83f25660a8838ed9d94766c77766f
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2023-01-12 15:14:51 +0530

    11 Navigation Bar for seller is made dynamic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menuType: string = 'default';
 
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {
+    this.route.events.subscribe((res: any) => {
+      // console.log(res.url);
+      if (res.url) {
+        if (localStorage.getItem('seller') && res.url.includes('seller')) {
+          // console.log('in seller area');
+          this.menuType = 'seller';
+        } else {
+          // console.log('in default area');
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
