4539c73969c980d9ecc3bd597ba449c179160a03
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2023-01-14 11:35:58 +0530

    19 git product autofill

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { ProductService } from './../services/product.service';
+import { Product } from './../data-type';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-seller-update-product',
   templateUrl: './seller-update-product.component.html',
   styleUrls: ['./seller-update-product.component.css'],
 })
-export class SellerUpdateProductComponent {
+export class SellerUpdateProductComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private product: ProductService) {}
+
+  productData: Product | undefined;
+
+  ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    // console.log(typeof productId);
+
+    if (productId) {
+      this.product.getProduct(productId).subscribe((data) => {
+        // console.log(data);
+        this.productData = data;
+      });
+    }
+  }
+
   submit(data: any) {}
 }
