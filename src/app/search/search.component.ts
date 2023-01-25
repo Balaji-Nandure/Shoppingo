198cc8b1b8824ef269ac3fcacb3ab0cd5916a8fe
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2023-01-25 12:14:46 +0530

    search functonality implemented

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Product } from './../data-type';
+import { ProductService } from './../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchResult: undefined | Product[];
 
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private product: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    // console.log(this.activeRoute);
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    // console.log(query);
+
+    query &&
+      this.product.searchProducts(query).subscribe((data) => {
+        // console.log(data);
+        this.searchResult = data;
+      });
+  }
 }
